f075cd23453809e6e126cc54beaf3fe8d27b9596
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-06-12 18:48:01 -0400

    Creando Models

---
+++ EFCoreTutorials/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCoreTutorials.Models
+{
+    class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+}
