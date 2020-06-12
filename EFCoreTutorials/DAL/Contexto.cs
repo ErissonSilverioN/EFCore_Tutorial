3a1da7faadb056923880548dace707433c926772
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-06-12 18:52:36 -0400

    Agregando Contexto

---
+++ EFCoreTutorials/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EFCoreTutorials.Models;
+
+namespace EFCoreTutorials.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Course> courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDb;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
